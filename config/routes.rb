cbdbeb7c5747f460419bdb80799d15fe6920ff08
crystal <noelani.villanueva@gmail.com>
2021-06-28 14:17:40 -0700

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   resources :notes
   resources :users, only: [:create]
-  post 'login', to: "auth#create"
+  post '/login', to: "auth#create"
   post '/signup', to: 'users#create'
   get 'persist', to: "auth#show"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
