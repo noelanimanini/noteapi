54e09b427f5b4bc7cece5f3de431f26bb3ea466c
crystal <noelani.villanueva@gmail.com>
2021-06-28 01:05:56 -0700

    set up backend

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  resources :users
   resources :notes
+  resources :users, only: [:create]
+  post 'login', to: "auth#create"
+  post '/signup', to: 'users#create'
+  get 'persist', to: "auth#show"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
